380b08b8cd218cfb30f43dd812df1b67b41ea611
Mehul Agarwal <agarwalmehul92@gmail.com>
2024-10-01 01:23:01 +0530

    Add Logs for Info

--- src/lib/prompts/awsS3BucketPrompt.ts
+++ src/lib/prompts/awsS3BucketPrompt.ts
@@ -8,10 +8,11 @@ export default async function awsS3BucketPrompt(
   if (bucketName) {
     return bucketName
   }
-  const buckets: string[] = new CliCommand(
-    'AWS S3 Bucket List',
-    `aws s3 ls --profile ${awsProfile} --recursive`
-  )
+
+  console.log('Fetching AWS S3 Buckets...')
+  const buckets: string[] = new CliCommand('AWS S3 Bucket List', `aws s3 ls`)
+    .appendArgs('profile', awsProfile)
+    .appendArgs('recursive')
     .exec(false)
     .toString()
     .split('\n')
