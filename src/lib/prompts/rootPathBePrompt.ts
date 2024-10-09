b34906d681e5d505e54f3eb1e4552c34aa9c4715
Mehul Agarwal <agarwalmehul92@gmail.com>
2024-10-09 23:15:44 +0530

    Fix rootPathBePrompt

--- src/lib/prompts/rootPathBePrompt.ts
+++ src/lib/prompts/rootPathBePrompt.ts
@@ -1,6 +1,7 @@
 import fs from 'fs'
 import inputPromptWithOptions from './inputPromptWithOptions'
 import { logger } from '../logger'
+import inputPrompt from './inputPrompt'
 
 export default async function rootPathBePrompt(projectRoot: string): Promise<{
   projectRoot: string
@@ -9,31 +10,24 @@ export default async function rootPathBePrompt(projectRoot: string): Promise<{
   devEnvFilePath: string
   sdksFolderPath: string
 }> {
-  let rootPath = projectRoot
-  let apiFolderPath = './api'
+  let rootPath = projectRoot || '.'
+  let apiFolderPath = `${rootPath}/api`
   let startServerFilePath = `${rootPath}/startServer.mjs`
   let devEnvFilePath = `${rootPath}/.env.development`
   let sdksFolderPath = `${apiFolderPath}/sdks`
 
-  if (!projectRoot) {
-    rootPath = await inputPromptWithOptions('Project Root Path:', ['.'])
+  let isValid = _validatePaths(rootPath, apiFolderPath)
+
+  if (!isValid && !projectRoot) {
+    rootPath = await inputPrompt('Project Root Path:')
     apiFolderPath = `${rootPath}/api`
     startServerFilePath = `${rootPath}/startServer.mjs`
     devEnvFilePath = `${rootPath}/.env.development`
     sdksFolderPath = `${apiFolderPath}/sdks`
+    isValid = _validatePaths(rootPath, apiFolderPath)
   }
 
-  if (!fs.existsSync(rootPath)) {
-    logger.fatal(
-      `[Error] BE Project does not exist at the location: ${rootPath}`
-    )
-    process.exit()
-  }
-
-  if (!fs.existsSync(apiFolderPath)) {
-    logger.fatal(
-      `[Error] Project API Folder does not exist at the location: ${apiFolderPath}`
-    )
+  if (!isValid) {
     process.exit()
   }
 
@@ -44,4 +38,22 @@ export default async function rootPathBePrompt(projectRoot: string): Promise<{
     devEnvFilePath,
     sdksFolderPath
   }
+}
+
+function _validatePaths(rootPath: string, apiFolderPath: string): boolean {
+  if (!fs.existsSync(rootPath)) {
+    logger.fatal(
+      `[Error] BE Project does not exist at the location: ${rootPath}`
+    )
+    return false
+  }
+
+  if (!fs.existsSync(apiFolderPath)) {
+    logger.fatal(
+      `[Error] Project API Folder does not exist at the location: ${apiFolderPath}`
+    )
+    return false
+  }
+
+  return true
 }
